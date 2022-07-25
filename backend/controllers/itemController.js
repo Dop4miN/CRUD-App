c919db9e96c1b829981210e5fec1ba62aa7dc792
Fabian Hoedl <f.hoedl@intencer.local>
2022-07-26 00:04:25 +0200

    bugfix: removed finding user by ID since it's already in the request

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -29,14 +29,12 @@ const putItem = asyncHandler(async (req, res) => {
     throw new Error("Item not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (item.user.toString() != user.id) {
+  if (item.user.toString() != req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -54,14 +52,12 @@ const deleteItem = asyncHandler(async (req, res) => {
     throw new Error("Item not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (item.user.toString() != user.id) {
+  if (item.user.toString() != req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
